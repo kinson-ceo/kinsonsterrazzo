34e8590f2b67f00a1fd1f5898cfa80bb11326e9d
kinson-ceo <kinsonsterrazzo@gmail.com>
2025-03-28 08:57:37 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("quote-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Your quote request has been submitted! We'll get back to you soon.");
+});
